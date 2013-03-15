312b95335da4204b8000b57eb6ebd3e8a48346fa
Apprentice <apprentice@devbootcamp.com>
2013-03-15 15:39:55 -0700

    uncommented seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,15 @@ User.create(:name => "Gavin St. Ours",
 						:email => "gavin@gmail.com",
 						:password => "gavin")
 
-# Deck.create(:name => "Jeopardy")
-# Deck.create(:name => "Jokes")
-# Deck.create(:name => "Math")
+Deck.create(:name => "Jeopardy")
+Deck.create(:name => "Jokes")
+Deck.create(:name => "Math")
 
-# File.open('db/jeopardy.txt').each_line do |line|
-# 	row = line.split(/\ *\t\ */)
-# 	begin
-# 		Card.create(:question => row[4], :answer => row[6], :value => row[8], :deck => Deck.find_or_create_by_name(row[2]))
-# 	rescue
-# 		puts "Invalid Row"
-# 	end
-# end
+File.open('db/jeopardy.txt').each_line do |line|
+	row = line.split(/\ *\t\ */)
+	begin
+		Card.create(:question => row[4], :answer => row[6], :value => row[8], :deck => Deck.find_or_create_by_name(row[2]))
+	rescue
+		puts "Invalid Row"
+	end
+end
