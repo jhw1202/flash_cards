dace687f95f18fac8eb44aa28eae6ab5a1f566e4
Apprentice <apprentice@devbootcamp.com>
2013-03-15 17:09:03 -0700

    added user authentication

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,7 @@
+before do
+  @current_user = get_current_user(session[:token])
+end
+
 get '/' do
   # Log in or sign up; user can't do anything until then.
   erb :index
@@ -6,23 +10,31 @@ end
 post '/login' do
   # take params to authenticate user
   # give session token
-  user = User.find_by_email_and_password(params[:email], params[:password])
+  user = User.authenticate(params[:email], params[:password])
   if user
-    session[:token] = user.token
+    assign_token(user)
     redirect '/home'
   else
-    redirect '/'
+    @errors = ['Invalid email/password']
+    erb :index
   end
 end
 
 post '/signup' do
   # take params and create new user
   # assign session token
-  User.create(params)
-  redirect '/home'
+  user = User.new(params[:user])
+  if user.save
+    assign_token(user)
+    redirect '/home'
+  else
+    puts @errors = user.errors.full_messages
+    erb :index
+  end
 end
 
 get '/home' do
+  redirect '/' unless @current_user
   # Choose a deck to start a round
   # See results from past rounds (link to '/history')
   @user = User.find_by_token(session[:token])
@@ -30,6 +42,7 @@ get '/home' do
 end
 
 get '/round' do
+  redirect '/' unless @current_user
   # Set a session[:deck_id] and session[:card_id]
   # Shows the user the next card in the deck
   # Takes a guess in an input field
@@ -38,6 +51,7 @@ get '/round' do
 end
 
 post '/guess' do
+  redirect '/' unless @current_user
   # Make a new guess object, taking in :round_id and :card_id
   # Compare guess to correct answer, store true/false
   # increment :card_id
@@ -45,6 +59,7 @@ post '/guess' do
 end
 
 get '/results' do
+  redirect '/' unless @current_user
   # Destroy session[:deck_id] and session[:card_id]
   # Give correct/incorrect score
   # List cards that had incorrect guesses
@@ -52,6 +67,7 @@ get '/results' do
 end
 
 get '/history' do
+  redirect '/' unless @current_user
   # Show user result history
   # (Deck name, correct/incorrect score, date played)
   erb :history
