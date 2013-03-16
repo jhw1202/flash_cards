c6075031f66c4197dd2275774654e75f4ea352fd
Apprentice <apprentice@devbootcamp.com>
2013-03-16 11:22:53 -0700

    fixed jquery selector to make a hrefs on other views to work

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('a').on('click', function(e) {
+  $('.toggle-link').on('click', function(e) {
     e.preventDefault();
     $('.index-form').removeClass('inactive');
     $(this).closest('.index-form').addClass('inactive');
